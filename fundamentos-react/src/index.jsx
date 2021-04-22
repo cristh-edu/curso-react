09fceb47d3f0ae3b578de056353dc3963a403746
Cristhian <contato.cristhian.slopes@gmail.com>
2021-04-22 15:48:14 -0300

    Primeiro Componente

---
+++ fundamentos-react/src/index.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Primeiro from './components/basicos/Primeiro'
+
+ReactDOM.render(
+    <>
+     <Primeiro></Primeiro>
+    </>,
+    document.getElementById('root'))
